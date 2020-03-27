8e903bb8fd535fe5f86fcc1ab8a386321125ba7c
qbroquetas <qbroquetas@outlook.com>
2020-03-27 10:55:42 +0100

    afegir les consultes fetes a l'exercici anterior

--- server/source/BBDD_Handler.h
+++ server/source/BBDD_Handler.h
@@ -2,15 +2,13 @@
 #define BBDD_HANDLER_H
 
 #define USRN_LENGTH 32
-#define GAME_LENGTH 32
+#define GAME_ID_LENGTH 8
 #define PASS_LENGTH 32
 
-//#include <mysql.h>
-
 int BBDD_connect();
-char* BBDD_find_winner(char game[GAME_LENGTH]);
+char* BBDD_ranking();
 char* BBDD_time_played(char username[USRN_LENGTH]);
-char* BBDD_find_characters(char game[GAME_LENGTH]);
+char* BBDD_find_characters(char game_id[GAME_ID_LENGTH]);
 int BBDD_add_user(char username[USRN_LENGTH], char passwd[PASS_LENGTH]);
 int BBDD_check_login (char username[USRN_LENGTH], char passwd[PASS_LENGTH]);
 int send_query();
