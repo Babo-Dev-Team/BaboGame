1bc13e555ee7f939eb00bcce98ef9200e3a8c428
Limax-cs <limax10000100100000@gmail.com>
2020-04-23 13:35:30 +0200

    Adaptació a shiva i shiva2

--- server/main.c
+++ server/main.c
@@ -369,8 +369,8 @@ int main(int argc, char *argv[])
 	// asocia el socket a cualquiera de las IP de la maquina. 
 	// htonl formatea el numero que recibe al formato necesario
 	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	// escucharemos en el port 9050
-	serv_addr.sin_port = htons(9098);
+	// escucharemos en el puerto 50084, 50085 i/o 50086
+	serv_addr.sin_port = htons(5084);
 	if (bind(sock_listen, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
 		printf ("Error al bind");
 	//La cola de requestes pendientes no podr? ser superior a 4
